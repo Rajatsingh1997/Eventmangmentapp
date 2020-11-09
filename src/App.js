20fa9208dce9d2eb147ee1f4fe6cb0c23bcb02d1
Rajat Singh <rajatrawat135@gmail.com>
2020-11-09 10:23:58 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
-// import Eventdetailform from "./components/Eventdetailform";
-// import { HashRouter, Route, Switch } from "react-router-dom";
-// import CalenderList from "./components/CalenderList";
-import Map from "./components/Map";
+import Eventdetailform from "./components/Eventdetailform";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import CalenderList from "./components/CalenderList";
+// import Map from "./components/Map";
 
 function App() {
   return (
     <>
-    {/* <HashRouter>
+    <HashRouter>
      <Switch>
       <Route path="/" exact component={Eventdetailform}/>
       <Route path="/calender" exact component={CalenderList}/>
     </Switch>
-    </HashRouter> */}
-    <Map/>
+    </HashRouter>
+    {/* <Map/> */}
     </>
   );
 }
--- src/components/Eventdetailform.js
+++ src/components/Eventdetailform.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
 import { Form, Button, Col } from "react-bootstrap";
-import Forms from "./Forms";
 import "./Eventdetailform.css";
 import Navbr from "./Navbr";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 
 export default function Eventdetailform(props) {
@@ -15,44 +14,44 @@ export default function Eventdetailform(props) {
     const [tilldate, setTilldate] = useState('');
 
 
-    const handlesubmit = (e) => {
-        let loginData = {
-            event: event.trim(),
-            detail: detail.trim(),
-          };
-          console.log(loginData);
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(event)
+        console.log(detail)
+        console.log(city)
+        console.log(fromdate)
+        console.log(tilldate)
+          
+        // if (event =="") {
+        //     console.log("enter event title" );
+        //     alert("enter event title");
+        //     e.preventDefault();
+        // }
+        // else if (detail =="") {
+        //     alert("enter event details");
+        //     e.preventDefault();
+        // }
+        // else if (city =="") {
+        //     alert("enter event details");
+        //     e.preventDefault();
+        // }
+        // else if (fromdate =="") {
+        //     alert("enter event details");
+        //     e.preventDefault();
+        // }
+        // else if (tilldate =="") {
+        //     alert("enter event details");
+        //     e.preventDefault();
+        // }
+        // else {
+        //     props.clndrList(event, detail, city, fromdate, tilldate)
+        //     setEvent('');
+        //     setDetail('');
+        //     setCity('');
+        //     setFromdate('');
+        //     setTilldate('');
 
-
-        if (event =="") {
-            console.log("enter event title" );
-            alert("enter event title");
-            e.preventDefault();
-        }
-        else if (detail =="") {
-            alert("enter event details");
-            e.preventDefault();
-        }
-        else if (city =="") {
-            alert("enter event details");
-            e.preventDefault();
-        }
-        else if (fromdate =="") {
-            alert("enter event details");
-            e.preventDefault();
-        }
-        else if (tilldate =="") {
-            alert("enter event details");
-            e.preventDefault();
-        }
-        else {
-            props.clndrList(event, detail, city, fromdate, tilldate)
-            setEvent('');
-            setDetail('');
-            setCity('');
-            setFromdate('');
-            setTilldate('');
-
-        }
+        // }
     }
 
 
@@ -62,7 +61,7 @@ export default function Eventdetailform(props) {
             <Navbr />
             <div className="formshows">
 
-                <Form onSubmit={(e) => handlesubmit}>
+                <Form onSubmit={onSubmit}>
 
 
                     <Form.Group controlId="formGridAddress1">
@@ -71,7 +70,7 @@ export default function Eventdetailform(props) {
                             value={event}
                             type="text"
                            
-                            onChange={(e) => { setEvent(e.target.value) }} />
+                            onChange={(e) => {setEvent(e.target.value) }} />
                     </Form.Group>
 
                     <Form.Group controlId="formGridAddress2">
@@ -80,13 +79,13 @@ export default function Eventdetailform(props) {
 
                            value={detail}
                             type="text"
-                            onChange={(e) => { setDetail(e.target.value) }} />
+                            onChange={(e) => {setDetail(e.target.value) }} />
                     </Form.Group>
 
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridCity"
                             type="city"
-                            onChange={(e) => { setCity(e.target.value) }}>
+                            onChange={(e) => {setCity(e.target.value) }}>
                             <Form.Label>City</Form.Label>
                             <Form.Control />
                         </Form.Group>
@@ -112,24 +111,25 @@ export default function Eventdetailform(props) {
 
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridEmail"
-                            onChange={(e) => { setFromdate(e.target.value) }}>
+                            onChange={(e) => {setFromdate(e.target.value) }}>
                             <Form.Label>From</Form.Label>
                             <Form.Control type="date" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword"
 
-                            onChange={(e) => { setTilldate(e.target.value) }}>
+                            onChange={(e) => {setTilldate(e.target.value) }}>
                             <Form.Label>Till</Form.Label>
                             <Form.Control type="date" placeholder="Password" />
                         </Form.Group>
                     </Form.Row>
-                    <Link to="/calender">
+                    {/* <Link to="/calender"> */}
                         <Button variant="success" 
                         className="rajat" 
-                        disabled={event && detail ? false : true}>
+                        disabled={event && detail ? false : true}
+                        onClick={onSubmit}>
                         Book Event</Button>{' '}
-                    </Link>
+                    {/* </Link> */}
                 </Form>              
             </div>
         </>
